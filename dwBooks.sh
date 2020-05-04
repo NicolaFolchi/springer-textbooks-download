c3aa055d16d32608bafd7142ef3c8e3906b2c06b
Nicola Folchi <nico93@live.unc.edu>
2020-05-04 00:02:24 -0400

    Adding compatibility for symbols on title

--- dwBooks.sh
+++ dwBooks.sh
@@ -11,7 +11,7 @@ do
 	# Because our initial URL will redirect, I will save the actual URL that contains HTML data
 	finalURL="$(curl -Ls -o /dev/null -w %{url_effective} "$URL")"
 	# Here I scrape the HTML to find the respective EBook name, I first remove the opening header tag and later I remove the closing tag from the String
-	bookName="$(curl -s "${finalURL}" | grep -o '<h1>[a-zA-Z ]*</h1>' | cut -c 5-)"
+	bookName="$(curl -s "${finalURL}" | grep -o '<h1>[a-zA-Z0-9 ®@&:;.,()+-]*</h1>' | cut -c 5-)"
 	bookName=${bookName::-5}
 	# Here I am going through the HTML and obtaining the href of the button that will direct us to the pdf file on the Springer server
 	pdfLink="$(curl -s "${finalURL}" | grep -m 1 -o '/content/pdf/[a-zA-Z0-9+-.%]*[.pdf]')"
